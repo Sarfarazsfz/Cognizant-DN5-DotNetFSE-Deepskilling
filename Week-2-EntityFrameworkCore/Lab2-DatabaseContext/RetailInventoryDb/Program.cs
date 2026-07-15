6dcc600b3c19e3ffc978dbcda831a8f00d9f5afb
Md Sarfaraz Alam <mdsarfarazalam669@gmail.com>
2026-07-15 17:31:18 +0530

    Completed EF Core Lab 6 update and delete records

--- Week-2-EntityFrameworkCore/Lab2-DatabaseContext/RetailInventoryDb/Program.cs
+++ Week-2-EntityFrameworkCore/Lab2-DatabaseContext/RetailInventoryDb/Program.cs
@@ -1,11 +1,37 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RetailInventoryDb;
 
-Console.WriteLine("===== LAB 5 : Retrieving Data =====");
+Console.WriteLine("===== LAB 6 : Update and Delete Records =====");
 
 await using var context = new AppDbContext();
 
-Console.WriteLine("\nAll Products:");
+var product =
+    await context.Products
+        .FirstOrDefaultAsync(p => p.Name == "Laptop");
+
+if (product != null)
+{
+    product.Price = 70000;
+
+    await context.SaveChangesAsync();
+
+    Console.WriteLine("Laptop price updated to ₹70000");
+}
+
+var toDelete =
+    await context.Products
+        .FirstOrDefaultAsync(p => p.Name == "Rice Bag");
+
+if (toDelete != null)
+{
+    context.Products.Remove(toDelete);
+
+    await context.SaveChangesAsync();
+
+    Console.WriteLine("Rice Bag deleted successfully");
+}
+
+Console.WriteLine("\nCurrent Products:");
 
 var products = await context.Products.ToListAsync();
 
@@ -13,15 +39,3 @@ foreach (var p in products)
 {
     Console.WriteLine($"{p.Name} - ₹{p.Price}");
 }
-
-Console.WriteLine();
-
-var product = await context.Products.FindAsync(1);
-
-Console.WriteLine($"Found: {product?.Name}");
-
-var expensive =
-    await context.Products
-        .FirstOrDefaultAsync(p => p.Price > 50000);
-
-Console.WriteLine($"Expensive: {expensive?.Name}");
